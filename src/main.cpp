54ff0dbd6d9e33c38db4e0ab1a3c092c463165ec
wimos-ai <willmoiser@gmail.com>
2024-04-30 10:04:44 -0500

    Added stuff to build SDL_Image in. Got basic file working

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,13 @@
 #include <cstdio>
 #include <utility>
-
+#include <iostream>
 
 #include <SDL.h>
+#include <SDL_image.h>
 
 #include "imgui.h"
 #include "imgui_impl_sdl2.h"
 #include "imgui_impl_sdlrenderer2.h"
-
-
 
 #include "Scenes.hpp"
 #include "Image.hpp"
@@ -26,9 +25,8 @@
 #error This backend requires SDL 2.0.17+ because of SDL_RenderGeometry() function
 #endif
 
-
 // Main code
-int main(int, char**)
+int main(int, char **)
 {
     // Setup SDL
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0)
@@ -49,18 +47,18 @@ int main(int, char**)
     const int WindowWidth = 1280;
     const int WindowHeight = 720;
 
-    HittableScene world{ emissionTest() };
+    HittableScene world{emissionTest()};
     Camera cam(Vec3D(13, 2, 3), Vec3D(-13, -2, -3), Vec3D(0, -1, 0), 10, WindowWidth, WindowHeight);
     CMRenderer ray_renderer(19, cam, world);
 
-    SDL_Window* window = SDL_CreateWindow("FTC Ballistics Program Mk. 9", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WindowWidth, WindowHeight, window_flags);
+    SDL_Window *window = SDL_CreateWindow("FTC Ballistics Program Mk. 9", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WindowWidth, WindowHeight, window_flags);
     if (window == nullptr)
     {
         printf("Error: SDL_CreateWindow(): %s\n", SDL_GetError());
         return -1;
     }
-    
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
     if (renderer == nullptr)
     {
         SDL_Log("Error creating SDL_Renderer!");
@@ -73,7 +71,7 @@ int main(int, char**)
     // Setup Dear ImGui context
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO();
+    ImGuiIO &io = ImGui::GetIO();
     (void)io;
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;  // Enable Gamepad Controls
@@ -104,7 +102,8 @@ int main(int, char**)
             ImGui_ImplSDL2_ProcessEvent(&event);
             if (event.type == SDL_QUIT)
                 done = true;
-            if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE && event.window.windowID == SDL_GetWindowID(window)) {
+            if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE && event.window.windowID == SDL_GetWindowID(window))
+            {
                 done = true;
             }
         }
@@ -118,7 +117,7 @@ int main(int, char**)
         static float theta = 55;
 
         static float effiency = 1;
-        static std::array<float, 2UL> startingPoint = { 0,0 };
+        static std::array<float, 2UL> startingPoint = {0, 0};
 
         // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
         {
@@ -143,14 +142,46 @@ int main(int, char**)
 
         std::vector<char> rendered_image = BMPImageSaver::serialize(ray_renderer.get_image());
 
-        SDL_RWops* sdl_buff =  SDL_RWFromMem(rendered_image.data(), rendered_image.size());
+        SDL_RWops *sdl_buff = SDL_RWFromMem(rendered_image.data(), rendered_image.size());
+        if (sdl_buff == NULL)
+        {
+            std::cout << "sdl_buff: " << SDL_GetError();
+            std::exit(-1);
+        }
 
-        //SDL_Texture* loaded_tex = IMG_LoadTexture_RW(renderer, sdl_buff, 1);
+        SDL_Texture *loaded_tex;
+
+        SDL_Surface * loaded_surface = IMG_LoadBMP_RW(sdl_buff);
+
+        if (loaded_surface) {
+            loaded_tex = SDL_CreateTextureFromSurface(renderer, loaded_surface);
+            SDL_FreeSurface(loaded_surface);
+        }else{
+            std::cout << "IMG_LoadBMP_RW: " << SDL_GetError();
+            std::exit(-1);
+        }
+
+        if (loaded_tex == NULL)
+        {
+            std::cout << "SDL_CreateTextureFromSurface: " << SDL_GetError();
+            std::exit(-1);
+        }
+
+        SDL_Rect texture_rect;
+        texture_rect.x = 0;            // the x coordinate
+        texture_rect.y = 0;            // the y coordinate
+        texture_rect.w = WindowWidth;  // the width of the texture
+        texture_rect.h = WindowHeight; // the height of the texture
+
+        if (SDL_RenderCopy(renderer, loaded_tex, NULL, &texture_rect) != 0)
+        {
+            std::cout << "SDL_RenderCopy: " << SDL_GetError();
+            std::exit(-1);
+        }
 
         // End Render Raytraced Image
 
-
-        ImGui_ImplSDLRenderer2_RenderDrawData(ImGui::GetDrawData());
+        // ImGui_ImplSDLRenderer2_RenderDrawData(ImGui::GetDrawData());
         SDL_RenderPresent(renderer);
     }
 
